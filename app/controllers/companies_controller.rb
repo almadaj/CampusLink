6b0229452349cea679872c036a6aca40814dfbcd
Juliano Almada <julianoalmada48@gmail.com>
2024-11-16 11:45:51 -0300

    feat(controllers): criação de controllers e rotas de Student, Internship e Company

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,46 @@
+class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+    render json: @companies
+  end
+
+  def show
+    @company = Company.find(params[:id])
+    render json: @company
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      render json: @company, status: :created
+    else
+      render json: @company.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    if @company.update(company_params)
+      render json: @company
+    else
+      render json: @company.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @company = Company.find(params[:id])
+    @company.destroy
+    render json: { "message": "Company deleted successfully" }
+  end
+
+  def interns_by_company
+    @company = Company.find(params[:id])
+    @interns = @company.internships.includes(:student).map(&:student)
+    render json: @interns
+  end
+
+  private
+  def company_params
+    params.require(:company).permit(:name, :cnpj, :address, :phone, :email)
+  end
+end
---
+++ app/controllers/internships_controller.rb
@@ -0,0 +1,40 @@
+class InternshipsController < ApplicationController
+  def index
+    @internships = Internship.all
+    render json: @internships
+  end
+
+  def show
+    @internship = Internship.find(params[:id])
+    render json: @internship
+  end
+
+  def create
+    @internship = Internship.new(internship_params)
+    if @internship.save
+      render json: @internship, status: :created
+    else
+      render json: @intership.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @internship = Internship.find(params[:id])
+    if @internship.update(internship_params)
+      render json @internship
+    else
+      render json @internship.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @internship = Internship.find(params[:id])
+    @internship.destroy
+      render json: { "message": "Internship deleted successfully" }
+  end
+
+  private
+  def internship_params
+    params.require(:internship).permit(:company_id, :student_id, :salary, :workload, :start_date, :end_date)
+  end
+end
