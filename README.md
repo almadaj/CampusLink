f0b524f2fb6d824b1a17a9bec200551b6c6b0c45
Juliano Almada <julianoalmada48@gmail.com>
2024-12-22 18:11:28 -0300

    chore(seeds): Criação de seeds e reescrita do README

--- README.md
+++ README.md
@@ -2,22 +2,58 @@
 
 Wait... There's a software being built
 
-Things you may want to cover:
+## Tecnologias Usadas
+* Ruby on Rails
+* PostgreSQL
+* Docker
+* Node.js
+* React, ver [Front-End](https://github.com/almadaj/view4rails)
 
-* Ruby version: Ruby 3.1.0 or higher
+## Pré-requisitos
+* Ruby 3.3.6
+* Ruby on Rails 8.0.0
+* PostgreSQL
+* Docker (opcional)
 
-* System dependencies
 
-* Configuration
+1. **Instalar dependências**
 
-* Database creation
+   - Para instalar as dependências do Ruby, execute:
+     ```bash
+     bundle install
+     ```
 
-* Database initialization
+2. **Configuração do Banco de Dados**
 
-* How to run the test suite
+   - Crie o banco de dados e rode as migrations com o seguinte comando:
+     ```bash
+     rails db:create
+     rails db:migrate
+     rails db:seed
+     ```
+3. **Configuração do Banco de Dados com Docker (opcional)**
 
-* Services (job queues, cache servers, search engines, etc.)
+   - Crie o banco de dados e rode as migrations com o seguinte comando:
+     ```bash
+        docker run --name campuslink_dev -e POSTGRES_PASSWORD=(SUASENHA) -e POSTGRES_DB=campuslink_dev -p 5432:5432 -d postgres
+     ```
 
-* Deployment instructions
+4. **Configurações de Variáveis de Ambiente**
 
-* ...
+   - Insira suas credenciais em:
+     ```bash
+        config/database.yml
+     ```
+
+5. **Rodando o Projeto**
+
+   - Inicie o Servidor Rails:
+     ```bash
+        rails s
+     ```
+
+   - Acessar aplicação:
+     ```bash
+        http://localhost:3000
+     ```
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,87 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+Internship.destroy_all
+Company.destroy_all
+Student.destroy_all
+User.destroy_all
+
+# Criando empresas
+15.times do
+  Company.create(
+    name: Faker::Company.name,
+    cnpj: Faker::Number.number(digits: 14).to_s.gsub(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '\1.\2.\3/\4-\5'),
+    address: Faker::Address.full_address,
+    email: Faker::Internet.email(domain: 'company.com'),
+    phone: Faker::PhoneNumber.phone_number_with_country_code
+  )
+end
+
+# Criando estudantes
+courses = [
+'Biomedicina',
+'Administração',
+'Direito',
+'Medicina',
+'Arquitetura e Urbanismo',
+'Psicologia',
+'Ciências Contábeis',
+'Sistemas de Informação',
+'Educação Física',
+'Farmácia',
+'Análise e Desenvolvimento de Sistemas',
+'Engenharia Civil',
+'Engenharia de Produção',
+'Enfermagem',
+'Nutrição',
+'Fisioterapia',
+'Gastronomia',
+'Odontologia',
+'Radiologia'
+]
+
+def generate_student_number
+  year = Time.now.year.to_s[2..3]
+  semester = rand(1..2)
+  random_number = rand(10000..99999)
+  "#{year}#{semester}#{random_number}"
+end
+
+15.times do
+  Student.create(
+    student_number: generate_student_number,
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    birth: Faker::Date.birthday(min_age: 18, max_age: 35),
+    phone: Faker::PhoneNumber.phone_number_with_country_code,
+    address: Faker::Address.full_address,
+    course: courses.sample
+  )
+end
+
+
+# Criando estágios
+companies = Company.pluck(:id)
+students = Student.pluck(:id)
+
+if companies.empty? || students.empty?
+  puts 'Não há empresas ou estudantes suficientes para criar estágios. Verifique os seeds das entidades relacionadas.'
+  exit
+end
+
+10.times do
+  Internship.create!(
+    company_id: companies.sample,
+    student_id: students.sample,
+    salary: rand(300..2000),
+    workload: [ 20, 30 ].sample,
+    start_date: Faker::Date.between(from: 2.years.ago, to: Date.today),
+    end_date: [ nil, Faker::Date.between(from: 2.years.ago, to: Date.today) ].sample
+  )
+end
+
+ # Criando usuário
+ User.create!(
+  name: 'John Doe',
+  email: 'john@example.com',
+  password_digest: 'admin123',
+  role: 1,
+ )
